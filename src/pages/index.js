cca528ffc3ea0c216c84b069b6baba32db7585fc
Robert Saunders <robertwsaunders@outlook.com>
2018-12-10 14:55:45 -0500

    Fix sentry error tracking to remove development

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,83 +18,98 @@ import HackerTestimonials from "../components/HackerTestimonials";
 import favicon from "../assets/img/icons/favicon.ico";
 import sharingImage from "../assets/img/logo/sharingImage.png";
 
-let environment = (process.env.NETLIFY_ENV = process.env.NODE_ENV);
-environment = environment === "deploy-preview" ? "development" : "staging";
 
-Sentry.init({
-  dsn: "https://fcd2394efcca4752b9b9b6742861e882@sentry.io/1317531",
-  environment: environment,
-  release: pjson.version
-});
+export default () => {
+  let environment = process.env.NETLIFY_ENV;
 
-export default () => (
-  <div css={{ overflowX: "hidden" }}>
-    <Helmet
-      title="QHacks | Queen's University | Winter 2019"
-      meta={[
-        {
-          name: "description",
-          content:
-            "QHacks is Queen’s University’s annual hackathon, happening February 1st - 3rd, 2019."
-        },
-        {
-          name: "keywords",
-          content: "Hackathon, Queen's University, Kingston, QHacks, Queensu"
-        },
-        {
-          name: "viewport",
-          content: "width=device-width, initial-scale=1.0"
-        },
-        {
-          property: "og:type",
-          content: "website"
-        },
-        {
-          property: "og:url",
-          content: "https://qhacks.io/"
-        },
-        {
-          property: "og:title",
-          content: "QHacks | Queen's University | Winter 2019"
-        },
-        {
-          property: "og:image",
-          content: sharingImage
-        },
-        {
-          property: "og:description",
-          content:
-            "QHacks is back for round four! We're bringing in students from all over North America to come together for a wild 36 hours - designing, developing, demoing, cup-stacking, and foosball-playing at our home base at Queen's University. Whether you're a first timer or a seasoned veteran, QHacks definitely has something to satisfy your interests. This weekend is about inclusiveness for all disciplines - students from any level of skill, from any field of study are encouraged to bring their innovative ideas for a weekend of creative problem solving! The only prerequisites for this weekend are a love for tech and drive to learn."
-        },
-        {
-          property: "og:site_name",
-          content: "QHacks"
-        },
-        {
-          property: "og:locale",
-          content: "en_US"
-        }
-      ]}
-      link={[
-        {
-          rel: "shortcut icon",
-          href: `${favicon}`
-        }
-      ]}
-    >
-      <html lang="en" />
-    </Helmet>
-    <div>
-      <Landing />
-      <MailingListSignup />
-      <About />
-      <HackerTestimonials />
-      <Speakers />
-      {/* <EventSchedule /> */}
-      <Sponsors />
-      <FAQs />
-      <Podcast />
-      <Footer />
+  switch (environment) {
+    case "deploy-preview":
+      environment = "development";
+    case "branch-deploy":
+      environment = "staging";
+    case "production":
+      environment = "production";
+    default:
+      environment = "development";
+  }
+
+  if (environment !== "development") {
+    Sentry.init({
+      environment,
+      release: pjson.version,
+      dsn: "https://fcd2394efcca4752b9b9b6742861e882@sentry.io/1317531"
+    });
+  }
+
+  return (
+    <div css={{ overflowX: "hidden" }}>
+      <Helmet
+        title="QHacks | Queen's University | Winter 2019"
+        meta={[
+          {
+            name: "description",
+            content:
+              "QHacks is Queen’s University’s annual hackathon, happening February 1st - 3rd, 2019."
+          },
+          {
+            name: "keywords",
+            content: "Hackathon, Queen's University, Kingston, QHacks, Queensu"
+          },
+          {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0"
+          },
+          {
+            property: "og:type",
+            content: "website"
+          },
+          {
+            property: "og:url",
+            content: "https://qhacks.io/"
+          },
+          {
+            property: "og:title",
+            content: "QHacks | Queen's University | Winter 2019"
+          },
+          {
+            property: "og:image",
+            content: sharingImage
+          },
+          {
+            property: "og:description",
+            content:
+              "QHacks is back for round four! We're bringing in students from all over North America to come together for a wild 36 hours - designing, developing, demoing, cup-stacking, and foosball-playing at our home base at Queen's University. Whether you're a first timer or a seasoned veteran, QHacks definitely has something to satisfy your interests. This weekend is about inclusiveness for all disciplines - students from any level of skill, from any field of study are encouraged to bring their innovative ideas for a weekend of creative problem solving! The only prerequisites for this weekend are a love for tech and drive to learn."
+          },
+          {
+            property: "og:site_name",
+            content: "QHacks"
+          },
+          {
+            property: "og:locale",
+            content: "en_US"
+          }
+        ]}
+        link={[
+          {
+            rel: "shortcut icon",
+            href: `${favicon}`
+          }
+        ]}
+      >
+        <html lang="en" />
+      </Helmet>
+      <div>
+        <Landing />
+        <MailingListSignup />
+        <About />
+        <HackerTestimonials />
+        <Speakers />
+        {/* <EventSchedule /> */}
+        <Sponsors />
+        <FAQs />
+        <Podcast />
+        <Footer />
+      </div>
     </div>
-  </div>
-);
+  );
+};
